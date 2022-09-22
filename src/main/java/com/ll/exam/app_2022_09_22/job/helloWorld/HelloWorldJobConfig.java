dad53ac554b0b92f958ca8ba302ee99e6863d1f1
ddmkim94 <ddmkim94@gmail.com>
2022-09-22 10:21:28 +0900

    helloWorldJob 실행

---
+++ src/main/java/com/ll/exam/app_2022_09_22/job/helloWorld/HelloWorldJobConfig.java
@@ -0,0 +1,45 @@
+package com.ll.exam.app_2022_09_22.job.helloWorld;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloWorldJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    // 메서드 이름 -> 빈의 이름 (유니크 해야한다)
+    @Bean
+    public Job helloWorldJob() {
+        // get(job의 이름)
+        return jobBuilderFactory.get("helloWorldJob")
+                .start(helloWorldStep1())
+                .build();
+    }
+
+    @Bean
+    public Step helloWorldStep1() {
+        return stepBuilderFactory.get("helloWorldStep1")
+                .tasklet(helloWorldTasklet())
+                .build();
+    }
+
+    // Tasklet: 하나의 작업 단위
+    @Bean
+    public Tasklet helloWorldTasklet() {
+        return (contribution, chunkContext) -> {
+            System.out.println("헬로월드!");
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+}
