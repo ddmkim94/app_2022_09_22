c9cce0d79de8a51556d22bfe32a76e1e41c37400
ddmkim94 <ddmkim94@gmail.com>
2022-09-26 10:29:47 +0900

    log 활성화, 파라미터 띄어쓰기

--- src/main/java/com/ll/exam/app_2022_09_22/job/helloWorld/WithParamJobConfig.java
+++ src/main/java/com/ll/exam/app_2022_09_22/job/helloWorld/WithParamJobConfig.java
@@ -1,6 +1,7 @@
 package com.ll.exam.app_2022_09_22.job.helloWorld;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class WithParamJobConfig {
 
     private final JobBuilderFactory jobBuilderFactory;
@@ -39,9 +41,9 @@ public class WithParamJobConfig {
     @StepScope
     public Tasklet withParamStep1Tasklet(
             @Value("#{jobParameters['name']}") String name,
-            @Value("#{jobParameters['age']}") int age) {
+            @Value("#{jobParameters['age']}") Long age) {
         return (contribution, chunkContext) -> {
-            System.out.println("WithParam 테스클릿 1, %s, %d".formatted(name, age));
+            log.debug("name = {}, age = {}", name, age);
             return RepeatStatus.FINISHED;
         };
     }
