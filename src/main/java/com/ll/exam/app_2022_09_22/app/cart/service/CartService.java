36b0e02101170e5dce77acf843b391c9486f4939
ddmkim94 <ddmkim94@gmail.com>
2022-09-26 13:26:44 +0900

    장바구니에 한 회원이 같은 상품옵션을 2번 이상 넣으면 개수만 증가

---
+++ src/main/java/com/ll/exam/app_2022_09_22/app/cart/service/CartService.java
@@ -0,0 +1,37 @@
+package com.ll.exam.app_2022_09_22.app.cart.service;
+
+import com.ll.exam.app_2022_09_22.app.cart.entity.CartItem;
+import com.ll.exam.app_2022_09_22.app.cart.repository.CartItemRepository;
+import com.ll.exam.app_2022_09_22.app.member.entity.Member;
+import com.ll.exam.app_2022_09_22.app.product.entity.ProductOption;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CartService {
+    private final CartItemRepository cartItemRepository;
+
+    public CartItem addItem(Member member, ProductOption productOption, int quantity) {
+        CartItem oldCartItem = cartItemRepository.findByMemberIdAndProductOptionId(member.getId(), productOption.getId()).orElse(null);
+
+        if (oldCartItem != null) {
+            oldCartItem.setQuantity(oldCartItem.getQuantity() + quantity);
+            cartItemRepository.save(oldCartItem);
+
+            return oldCartItem;
+        }
+        CartItem cartItem = CartItem.builder()
+                .member(member)
+                .productOption(productOption)
+                .quantity(quantity)
+                .build();
+
+        cartItemRepository.save(cartItem);
+        return cartItem;
+    }
+}
